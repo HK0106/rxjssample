666bf8317ec540774edf23637100eb1c163f4046
maidx <hiro27060106@gmail.com>
2023-10-02 16:14:01 +0700

    Update

--- rxjs-playground/src/index.ts
+++ rxjs-playground/src/index.ts
@@ -1,10 +1,24 @@
 import { Observable } from 'rxjs';
+//
+// const someObservable$ = new Observable<string>(subscriber => {
+//   subscriber.next('Alice');
+//   subscriber.next('Ben');
+//   subscriber.next('Charlie');
+//   subscriber.complete();
+// });
+//
+// console.log("subscription 1 ")
+// someObservable$.subscribe(value => console.log("subscription 1 exec ",value));
+//
+// setTimeout(() => {
+//   console.log("subscription 2 ")
+//   someObservable$.subscribe(value => console.log("subscription 2 exec ",value));
+// }, 1000)
 
-const someObservable$ = new Observable<string>(subscriber => {
-  subscriber.next('Alice');
-  subscriber.next('Ben');
-  subscriber.next('Charlie');
-  subscriber.complete();
+const observable$ = new Observable(subscriber => {
+  console.log('Observable executed!');
 });
 
-someObservable$.subscribe(value => console.log(value));
+console.log("Before subscribe!");
+observable$.subscribe()
+console.log("After subscribe!");
