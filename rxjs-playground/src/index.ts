e569fd0588cbd304ebf81c550bfafd20d6c24b3f
maidx <hiro27060106@gmail.com>
2023-10-02 17:21:04 +0700

    Update

--- rxjs-playground/src/index.ts
+++ rxjs-playground/src/index.ts
@@ -14,30 +14,52 @@ import { Observable } from 'rxjs';
 //   console.log("subscription 2 ")
 //   someObservable$.subscribe(value => console.log("subscription 2 exec ",value));
 // }, 1000)
+//
+// const observable$ = new Observable<string>(subscriber => {
+//   console.log('Observable executed!');
+//   subscriber.next("Alpha");
+//   subscriber.next("Bravo");
+//   setTimeout(() => {
+//     subscriber.next("Charlie");
+//     subscriber.next("Delta");
+//     // subscriber.complete();
+//   }, 2000);
+//
+//   setTimeout(() => {
+//     subscriber.error(new Error('Failure'))
+//   }, 4000);
+//
+//   return () => {
+//     console.log("Teardown");
+//   }
+// });
+//
+// console.log("Before subscribe!");
+// observable$.subscribe({
+//   next: value => {console.log("observable$.subscribe next 1", value)},
+//   error: err => {console.error("observable$.subscribe error 1", err.message)},
+//   complete : () => { console.log("observable$.subscribe complete 1")}
+// })
+// console.log("After subscribe!");
 
-const observable$ = new Observable<string>(subscriber => {
-  console.log('Observable executed!');
-  subscriber.next("Alpha");
-  subscriber.next("Bravo");
-  setTimeout(() => {
-    subscriber.next("Charlie");
-    subscriber.next("Delta");
-    // subscriber.complete();
-  }, 2000);
 
-  setTimeout(() => {
-    subscriber.error(new Error('Failure'))
-  }, 4000);
+const interval$ = new Observable<number>(subscriber => {
+  let count = 1;
+
+  const interval = setInterval(() => {
+    console.log("emmit", count);
+    subscriber.next(count++);
+  }, 2000)
 
   return () => {
-    console.log("Teardown");
+    clearInterval(interval);
+    console.log("Teardown!")
   }
-});
-
-console.log("Before subscribe!");
-observable$.subscribe({
-  next: value => {console.log("observable$.subscribe next 1", value)},
-  error: err => {console.error("observable$.subscribe error 1", err.message)},
-  complete : () => { console.log("observable$.subscribe complete 1")}
 })
-console.log("After subscribe!");
+
+const subscription = interval$.subscribe(value => {console.log(value)});
+
+setTimeout(() => {
+  console.log("unsubscribe!");
+  subscription.unsubscribe();
+}, 7000)
