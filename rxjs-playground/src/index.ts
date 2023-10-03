ce78dfb2d3b0af1aa988fe3a5fbd7f8af098ff1e
maidx <hiro27060106@gmail.com>
2023-10-03 11:01:25 +0700

    rxjs of

--- rxjs-playground/src/index.ts
+++ rxjs-playground/src/index.ts
@@ -63,29 +63,63 @@
 //   console.log("unsubscribe!");
 //   subscription.unsubscribe();
 // }, 7000)
+//
+//
+// import { fromEvent, interval, merge } from 'rxjs';
+// import { map, filter, catchError, mergeMap, scan } from 'rxjs/operators';
+//
+// // Simulated IoT data streams
+// const device1Stream = interval(1000).pipe(map(() => Math.random() * 100));
+// const device2Stream = interval(1500).pipe(map(() => Math.random() * 50));
+//
+// // Merge data streams from multiple devices
+// const mergedStream = merge(device1Stream, device2Stream);
+//
+// // Calculate and display average data value in real-time
+// mergedStream
+//     .pipe(
+//         catchError((err) => {
+//           console.error('Error:', err);
+//           return err; // Continue with the error
+//         }),
+//         scan((acc, val: number) => ({ sum: acc.sum + val, count: acc.count + 1 }), { sum: 0, count: 0 }),
+//         map((avg) => avg.sum / avg.count)
+//     )
+//     .subscribe((average) => {
+//       console.log('Real-time Average:', average);
+//       // Update the dashboard UI with the average value
+//     });
 
+import { Observable, of } from "rxjs";
 
-import { fromEvent, interval, merge } from 'rxjs';
-import { map, filter, catchError, mergeMap, scan } from 'rxjs/operators';
+of('Alpha', 'Bravo', 'Charlie', 'Delta').subscribe({
+    next: value => console.log("of subscribe value", value),
+    complete: () => console.log(" of Complete!")
+})
 
-// Simulated IoT data streams
-const device1Stream = interval(1000).pipe(map(() => Math.random() * 100));
-const device2Stream = interval(1500).pipe(map(() => Math.random() * 50));
+ourOwnOf('Alpha', 'Bravo', 'Charlie', 'Delta').subscribe({
+    next: value => console.log("ourOwnOf subscribe value", value),
+    complete: () => console.log("ourOwnOf Complete!")
+})
 
-// Merge data streams from multiple devices
-const mergedStream = merge(device1Stream, device2Stream);
+const name$ = new Observable<string>(subscriber => {
+    subscriber.next('Alpha');
+    subscriber.next('Bravo');
+    subscriber.next('Charlie');
+    subscriber.next('Delta');
+    subscriber.complete();
+});
 
-// Calculate and display average data value in real-time
-mergedStream
-    .pipe(
-        catchError((err) => {
-          console.error('Error:', err);
-          return err; // Continue with the error
-        }),
-        scan((acc, val: number) => ({ sum: acc.sum + val, count: acc.count + 1 }), { sum: 0, count: 0 }),
-        map((avg) => avg.sum / avg.count)
-    )
-    .subscribe((average) => {
-      console.log('Real-time Average:', average);
-      // Update the dashboard UI with the average value
-    });
+name$.subscribe({
+    next: value => console.log("subscribe value", value),
+    complete: () => console.log("Subscribe Complete!")
+})
+
+function ourOwnOf(...args: string[]): Observable<string> {
+    return new Observable<string>(subscriber => {
+        for (let i = 0; i < args.length; i++) {
+            subscriber.next(args[i])
+        }
+        subscriber.complete()
+    })
+}
